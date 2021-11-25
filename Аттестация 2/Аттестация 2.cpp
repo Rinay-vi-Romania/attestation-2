101888d4d4d60a7c6862dd5591d1a25280136aa0
Rinay-vi-Romania <karpitsky8@gmail.com>
2021-11-25 12:40:37 +0300

    Добавьте файлы проекта.

---
+++ Аттестация 2/Аттестация 2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+        int main()
+    {
+            string path = "file.txt";
+                const int N = 5;
+            int mass[N] = { 0 };
+            ifstream file;
+            file.open(path);
+            if (!file)
+                return 1;
+
+            for (int i = 0; i < N; i++)
+            {
+                file >> mass[i];
+                cout << mass[i];
+
+            }
+            
+        file.close();
+    return 0;
+      
+       
+        }
